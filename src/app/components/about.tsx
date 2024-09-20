6e982a8614c52417098065755f590e87406f784d
jwsvengit7 <chiorlujack@gmail.com>
2024-09-20 20:43:48 +0100

    ALL

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -16,7 +16,6 @@ export default function About({number}:{number:number}) {
                        
                     {number !=0 ? `${description.substring(0, number)}...` : description}
 
-
                     </p>
 
 
